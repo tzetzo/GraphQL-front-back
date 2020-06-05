27c18d1c9021927ed5431ec21b3baf6afdcd761d
tzetzo <tzvetanmarinov@yahoo.com>
2020-06-05 16:01:32 +0300

    custom network interface to tell ApolloClient to send Cookies with all requests to the Back-End

--- client/index.js
+++ client/index.js
@@ -1,16 +1,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ApolloClient from "apollo-client";
+import ApolloClient, { createNetworkInterface } from "apollo-client"; //makes the requests to the back-end server
 import { ApolloProvider } from "react-apollo";
+import { Router, hashHistory, Route } from "react-router";
+import App from "./components/App";
+
+//create our custom network interface
+const networkInterface = createNetworkInterface({
+  uri: "/graphql", //confirm the endpoint
+  opts: {
+    credentials: "same-origin" //tell ApolloClient to send Cookies when making requests to the back-end
+  }
+});
 
 const client = new ApolloClient({
+  networkInterface,
   dataIdFromObject: o => o.id // to make Apollo identify every record we fetch; we should always include the id in our queries
 });
 
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Auth Starter</div>
+      <Router history={hashHistory}>
+        <Route path="/" component={App}>
+          {/*
+          <Route path="/signup" component={SignUp} />
+          <Route path="/signin" component={SignIn} />*/}
+        </Route>
+      </Router>
     </ApolloProvider>
   );
 };
